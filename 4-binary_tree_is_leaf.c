e4bd9a34d65c7649ff47a81605b1307c5123d4c0
Epicder <epicder11@gmail.com>
2024-04-04 12:56:55 +0000

    commit

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,13 +9,13 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-  if (!node)
-    {
-      return (0);
-    }
-  if (node->right == NULL && node->left == NULL)
-    {
-      return (1);
-    }
-  return (0);
-    }
+if (!node)
+{
+return (0);
+}
+if (node->right == NULL && node->left == NULL)
+{
+return (1);
+}
+return (0);
+}
