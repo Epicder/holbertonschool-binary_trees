0862634516b0c0dc7a4d45d0fed7434d97709d01
matthew25072005 <8855@holbetronstudents.com>
2024-04-08 18:06:09 +0000

    commit

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,11 +8,14 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t left_depth;
+	size_t right_depth;
+
 	if (!tree)
 		return (0);
 
-	size_t left_depth = binary_tree_depth(tree->left);
-	size_t right_depth = binary_tree_depth(tree->right);
+	left_depth = binary_tree_depth(tree->left);
+	right_depth = binary_tree_depth(tree->right);
 
 	return (left_depth > right_depth ? left_depth + 1 : right_depth + 1);
 }
