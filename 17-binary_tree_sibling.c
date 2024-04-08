047c6d1b37ac9fe02d55f344f710e1a83a502244
matthew25072005 <8855@holbetronstudents.com>
2024-04-08 16:57:18 +0000

    commit

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,12 +10,16 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *parent = node->parent;
+
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
 
-	if (parent->left == node)
+	if (parent->left == node && parent->right != NULL)
 		return parent->right;
-	else
+
+	if (parent->right == node && parent->left != NULL)
 		return parent->left;
+
+	return (NULL);
 }
