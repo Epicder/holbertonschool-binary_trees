23f1779ad86cf848236fe7e97d6aa22515bbfaac
matthew25072005 <8855@holbetronstudents.com>
2024-04-08 18:28:31 +0000

    commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,22 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
-/**
- * binary_tree_balance - measures the balance
- * @tree: puntero al nodo root
- * Return: balance
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-int left_height, right_height;
-if (!tree)
-{
-return (0);
-}
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-return (left_height - right_height);
-}
+
 /**
  * binary_tree_height - retorna la height de un arbol
  * @tree: puntero a nodo root
@@ -24,16 +9,35 @@ return (left_height - right_height);
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left = 0;
-	size_t height_right = 0;
+		size_t height_left = 0;
+		size_t height_right = 0;
 
-	if (tree == NULL)
+		if (tree == NULL)
+		{
+			return (0);
+		}
+
+		height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return (height_left > height_right ? height_left : height_right);
+}
+
+/**
+ * binary_tree_balance - measures the balance
+ * @tree: puntero al nodo root
+ * Return: balance
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (!tree)
 	{
 		return (0);
 	}
-
-	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return (height_left > height_right ? height_left : height_right);
+	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	return (left_height - right_height);
 }
+
